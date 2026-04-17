dd8dee6bf7eaa108464f0ecd9139971c8b56b760
y <liyuan275@outlook.com>
2026-04-18 00:50:27 +0930

    feat: update about

--- src/components/About/AboutMe.tsx
+++ src/components/About/AboutMe.tsx
@@ -5,26 +5,8 @@ export default function AboutMe() {
         className="bg-purple-300 m-4 rounded-2xl p-15 
       border-6 border-b-12 border-r-12 text-bold text-[clamp(1rem,1vw,1.5rem)]"
       >
-        <br />
-
         <p>
-          I&apos;m a second-year Computing & Innovation student at the University of
-          Adelaide, passionate about creating useful and interesting tools and
-          websites. Blending curiosity with logic, I enjoy tackling challenges
-          that push me to grow while keeping my code clean and human-readable.
-        </p>
-        <br />
-        <p>
-          I&apos;m at my best when I&apos;m learning by doing, collaborating with
-          teammates, or finding smarter ways to solve problems. Outside of
-          coding, you can catch me deep in a chess match or immersed in a video
-          game world.
-        </p>
-        <br />
-        <p>
-          Curious, logical, and a true team player—I&apos;m now seeking internship
-          opportunities where I can contribute my skills, gain hands-on
-          experience, and keep leveling up as a developer.
+        Curious by nature, I'm a Computing & Innovation student at the University of Adelaide who loves building next-gen tools, exploring DevOps, and diving into open source. Outside of code, you'll find me at a chessboard or lost in a video game.
         </p>
       </div>
     </>
